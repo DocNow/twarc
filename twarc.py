a035bdee7c78b1b3cdcce21c901901ffd2dfaa4f
Ed Summers <ehs@pobox.com>
2013-01-14 17:31:53 +0000

    latest

--- twarc.py
+++ twarc.py
@@ -2,11 +2,11 @@
 
 import sys
 import json
+import time
 import config
 import oauth2
 import logging
 
-logging.basicConfig(filename="archive.log", level=logging.INFO)
 
 consumer = oauth2.Consumer(key=config.consumer_key, secret=config.consumer_secret)
 token = oauth2.Token(config.access_token, config.access_token_secret)
@@ -23,16 +23,20 @@ def sleep_till(t):
 def search(q, url=None, rate_limit_remaining=None, rate_limit_reset=None):
     if rate_limit_remaining != None and rate_limit_remaining == 0:
         sleep_till(rate_limit_reset)
+        rate_limit_remaining = None
+        rate_limit_reset = None
 
     # fetch some results
     if not url:
         url = "https://api.twitter.com/1.1/search/tweets.json?q=%s" % q
+
+    logging.info("fetching %s", url)
     resp, content = client.request(url)
 
     # set rate limit info if not known
     if rate_limit_remaining == None:
-        rate_limit_remaining = resp["x-rate-limit-remaining"]
-        rate_limit_reset = resp["x-rate-limit-reset"]
+        rate_limit_remaining = int(resp["x-rate-limit-remaining"])
+        rate_limit_reset = int(resp["x-rate-limit-reset"])
     else:
         rate_limit_remaining -= 1
 
@@ -50,13 +54,17 @@ def search(q, url=None, rate_limit_remaining=None, rate_limit_reset=None):
             yield status
 
 def archive(statuses, filename):
-    fh = open("archive.json", "w")
-    for status in search_results:
-        url = "http://twitter.com/%s/status/%s"status["user"]["screen_name"], status["id_str"]
+    fh = open(filename, "a")
+    for status in statuses:
+        url = "http://twitter.com/%s/status/%s" % (status["user"]["screen_name"], status["id_str"])
         logging.info("archived %s", url)
-        fh.write(dumps(status))
+        fh.write(json.dumps(status))
+        fh.write("\n")
 
 if __name__ == "__main__":
     q = sys.argv[1]
+    if len(sys.argv) > 2:
+        max_id =sys.argv[2]
+    logging.basicConfig(filename="%s.log" % q, level=logging.INFO)
     filename = "%s.json" % q
     archive(search(q), filename)
