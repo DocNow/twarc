9316dfb06fa9c63f3baa85de739dee7ea5c3dc82
nruest <ruestn@gmail.com>
2013-10-23 17:49:24 -0400

    elegant geojson thanks to advice from @mjsuhonos!

--- utils/geojson.py
+++ utils/geojson.py
@@ -3,37 +3,38 @@
 import json
 import fileinput
 import dateutil.parser
-import geojson
 
-print"""
-{
-  "features" : [
-"""
+latitude = []
+longitude = []
+name = []
+screen_name = []
 
 for line in fileinput.input():
     tweet = json.loads(line)
     if tweet["geo"]:
-      #coordinates = geojson.Point(tweet["geo"]["coordinates"])
-      latitude = (tweet["geo"]["coordinates"][0])
-      longitude = (tweet["geo"]["coordinates"][1])
-      name = (tweet["user"]["name"]).encode('utf-8')
-      screen_name = (tweet["user"]["screen_name"]).encode('utf-8')
-      print """
-      {
-        "geometry" : {
-          "coordinates" : [
-            "%s",
-            "%s"
-            ],
-          "type" : "Point"
-        },
-        "type" : "Feature",
-        "properties" : {
-          "Name" : "%s",
-          "Screen name" : "%s"
-        }
-      },""" % (latitude, longitude, name, screen_name)
-print """
-  ],
-  "type" : "FeatureCollection"
-}"""
+      longitude.append(tweet["geo"]["coordinates"][0])
+      latitude.append(tweet["geo"]["coordinates"][1])
+      name.append((tweet["user"]["name"]).encode('utf-8'))
+      screen_name.append((tweet["user"]["screen_name"]).encode('utf-8'))
+
+i = 0
+features = [] 
+while i < len(name):
+  feature = {"geometry" : {
+    "coordinates" : [
+      latitude[i],
+      longitude[i]
+      ],
+    "type" : "Point"
+    },
+    "type" : "Feature",
+    "properties" : {
+      "Name" : name[i],
+      "Screen name" : screen_name[i]
+      }
+    }
+  features.append(feature)
+  i = i + 1
+
+geojson = ({ "features" : features, "type" : "FeatureCollection" })
+print(json.dumps(geojson, indent=2))
