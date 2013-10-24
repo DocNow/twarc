8241cd7a6e35cedad012506d7e1cdfcb7a39b8b9
Ed Summers <ehs@pobox.com>
2013-10-24 16:29:13 -0400

    simplified a bit

--- utils/geojson.py
+++ utils/geojson.py
@@ -2,39 +2,33 @@
 
 import json
 import fileinput
-import dateutil.parser
 
-latitude = []
-longitude = []
-name = []
-screen_name = []
+features = []
 
 for line in fileinput.input():
     tweet = json.loads(line)
     if tweet["geo"]:
-      longitude.append(tweet["geo"]["coordinates"][0])
-      latitude.append(tweet["geo"]["coordinates"][1])
-      name.append((tweet["user"]["name"]).encode('utf-8'))
-      screen_name.append((tweet["user"]["screen_name"]).encode('utf-8'))
+        features.append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    tweet["geo"]["coordinates"][1],
+                    tweet["geo"]["coordinates"][0]
+                ],
+            },
+            "properties": {
+                "name": tweet["user"]["name"],
+                "screen_name": tweet["user"]["screen_name"],
+                "created_at": tweet["created_at"],
+                "text": tweet["text"],
+                "profile_image_url": tweet["user"]["profile_image_url"],
+                "url": "http://twitter.com/%s/status/%s" % (
+                    tweet["user"]["screen_name"], 
+                    tweet["id_str"],
+                )
+            }
+        })
 
-i = 0
-features = [] 
-while i < len(name):
-  feature = {"geometry" : {
-    "coordinates" : [
-      latitude[i],
-      longitude[i]
-      ],
-    "type" : "Point"
-    },
-    "type" : "Feature",
-    "properties" : {
-      "Name" : name[i],
-      "Screen name" : screen_name[i]
-      }
-    }
-  features.append(feature)
-  i = i + 1
-
-geojson = ({ "features" : features, "type" : "FeatureCollection" })
-print(json.dumps(geojson, indent=2))
+geojson = {"type" : "FeatureCollection", "features": features}
+print json.dumps(geojson, indent=2)
