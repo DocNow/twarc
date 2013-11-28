0dd04c7597e76eb32202b0bf82d0034b4aa0c992
Peter Binkley <peter.binkley@ualberta.ca>
2013-11-28 12:07:39 -0700

    added lookup when even expanded_url is a shortened url

--- utils/urls.py
+++ utils/urls.py
@@ -3,11 +3,22 @@
 import json
 import fileinput
 import dateutil.parser
+import requests
+
+shorteners = "http://bit.ly/", "http://goo.gl/", "http://t.co/", "http://tinyurl.com/", "http://is.gd/", "http://wp.me/", "http://fb.me/", "http://ow.ly/"
 
 for line in fileinput.input():
     tweet = json.loads(line)
     for url in tweet["entities"]["urls"]:
         if url["expanded_url"]:
-            print url["expanded_url"]
+            baseurl = url["expanded_url"]
         else:
-            print url
+            baseurl = url
+        if baseurl.startswith(shorteners):
+        	resp = requests.head(baseurl)
+        	if "Location" in resp.headers:
+        		print resp.headers["Location"]
+        	else:
+        		print baseurl
+        else:
+	        print baseurl
