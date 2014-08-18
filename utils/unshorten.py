601d66049e6432ceefbf3347447bea7cc3c6c7b9
Ed Summers <ehs@pobox.com>
2014-08-18 10:28:28 +0000

    now does concurrent lookups with multiprocessing

--- utils/unshorten.py
+++ utils/unshorten.py
@@ -13,38 +13,45 @@ import logging
 import requests
 import fileinput
 import urlparse
+import multiprocessing
+
+# number of urls to look up in parallel
+POOL_SIZE = 10
 
 logging.basicConfig(filename="unshorten.log", level=logging.INFO)
 
 cache = {}
+
 def unshorten(url):
     if url in cache:
         return cache[url]
     try:
         # let requests handle redirects
-        resp = requests.get(url)
+        resp = requests.get(url, timeout=5)
         cache[url] = resp.url
         return unshorten(resp.url)
     except Exception as e:
         logging.error("lookup failed for %s: %s", url, e)
         return url
 
+def rewrite_line(line):
+    tweet = json.loads(line)
+    for url_dict in tweet["entities"]["urls"]:
+        if "expanded_url" in url_dict:
+            url = url_dict["expanded_url"]
+        else:
+            url = url_dict['url']
+
+        unshortened_url = unshorten(url)
+        logging.info("unshortened %s to %s", url, unshortened_url)
+
+        # add new key to the json
+        url_dict['unshortened_url'] = unshortened_url
+    return tweet
 
 def main():
-    for line in fileinput.input():
-        tweet = json.loads(line)
-        for url_dict in tweet["entities"]["urls"]:
-            if "expanded_url" in url_dict:
-                url = url_dict["expanded_url"]
-            else:
-                url = url_dict['url']
-
-            unshortened_url = unshorten(url)
-            logging.info("unshortened %s to %s", url, unshortened_url)
-
-            # add new key to the json
-            url_dict['unshortened_url'] = unshortened_url
-
+    pool = multiprocessing.Pool(POOL_SIZE)
+    for tweet in pool.imap(rewrite_line, fileinput.input()):
         print json.dumps(tweet).encode('utf8')
 
 if __name__ == "__main__":
