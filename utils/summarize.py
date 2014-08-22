3707b2f92e2b876dcc8c05c41b52146600907f56
Ed Summers <ehs@pobox.com>
2014-08-22 10:33:40 +0000

    order output

--- utils/summarize.py
+++ utils/summarize.py
@@ -18,13 +18,23 @@ def first_last(filename):
     last = json.loads(line)
     return first, last, count
 
+files = []
 for filename in sys.argv[1:]:
     if filename.endswith('.json'):
         # note: tweets are in reverse chronological order
         # so the first tweet is the latest ...
         first, last, count = first_last(filename)
-        print
-        print filename 
-        print "  start: %s [%s]" % (last["id_str"], last["created_at"])
-        print "  end:   %s [%s]" % (first["id_str"], first["created_at"])
-        print "  total: %s" % count
+        files.append({
+            "filename": filename,
+            "first": first, 
+            "last": last, 
+            "count": count
+        })
+
+files.sort(lambda a, b: cmp(a['first']['id_str'], b['first']['id_str']))
+for f in files:
+    print
+    print f['filename']
+    print "  start: %s [%s]" % (f['last']["id_str"], f['last']["created_at"])
+    print "  end:   %s [%s]" % (f['first']["id_str"], f['first']["created_at"])
+    print "  total: %s" % f['count']
