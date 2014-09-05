4137166d3ad56ad475c4b8f9769bf4a7130f3199
Ed Summers <ehs@pobox.com>
2014-09-05 05:19:56 -0400

    pypi release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'twarc',
-    version = '0.0.3',
+    version = '0.0.4',
     url = 'http://github.com/edsu/twarc',
     author = 'Ed Summers',
     author_email = 'ehs@pobox.com',
