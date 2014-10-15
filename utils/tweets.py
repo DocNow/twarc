3fda015bdd8903ef464c028fee36ff9e7eebb085
Ed Summers <ehs@pobox.com>
2014-10-15 00:47:13 +0000

    separate user name

--- utils/tweets.py
+++ utils/tweets.py
@@ -7,7 +7,7 @@ import dateutil.parser
 for line in fileinput.input():
     tweet = json.loads(line)
     created_at = dateutil.parser.parse(tweet["created_at"])
-    print ("[%s] @%s %s (%s)" % (
+    print ("[%s] @%s: %s (%s)" % (
         created_at.strftime("%Y-%m-%d %H:%M:%S"),
         tweet["user"]["screen_name"],
         tweet["text"],
