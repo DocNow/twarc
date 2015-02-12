2b4bd8d78197984a43ba403bc36b82b795ae19c1
Ed Summers <ehs@pobox.com>
2015-02-12 12:51:06 -0500

    updated for new twarc.py

--- utils/tweet.py
+++ utils/tweet.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
+"""
+Fetch a single tweet as JSON using its id.
+"""
+
 import sys
 import json
 import twarc
 
 tweet_id = sys.argv[1]
-tw = twarc.TwitterClient()
-tweet = tw.fetch('https://api.twitter.com/1.1/statuses/show/%s.json' % tweet_id)
+tw = twarc.Twarc()
+tweet = tw.get('https://api.twitter.com/1.1/statuses/show/%s.json' % tweet_id)
 
-print json.dumps(tweet, indent=2)
+print json.dumps(tweet.json(), indent=2)
 
 
 
