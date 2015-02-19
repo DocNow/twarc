15677840dd76a0668fd5abf01e5fc3c3b02823c7
Ed Summers <ehs@pobox.com>
2015-02-19 16:09:51 -0500

    new release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class PyTest(Command):
 
 setup(
     name='twarc',
-    version='0.2.2',
+    version='0.2.3',
     url='http://github.com/edsu/twarc',
     author='Ed Summers',
     author_email='ehs@pobox.com',
