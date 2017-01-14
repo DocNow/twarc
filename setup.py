81d9a71c6f17c028deecbba2a8a936c1b0c5f067
Ed Summers <ehs@pobox.com>
2017-01-14 06:46:25 -0500

    upping version for release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import join
 from setuptools import setup
 
 # Also in twarc.py
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 if sys.version_info[0] < 3:
     dependencies = open(join('requirements', 'python2.txt')).read().split()
