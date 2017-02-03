b0f499e3d83c29016785c4cdf702923f0f44c5e7
Ed Summers <ehs@pobox.com>
2017-02-03 11:04:47 -0500

    removed some duplication. fixes #145

--- setup.py
+++ setup.py
@@ -12,35 +12,22 @@ if sys.version_info[0] < 3:
 else:
     dependencies = open(join('requirements', 'python3.txt')).read().split()
 
+if os.name == "nt":
+    scripts = ['twarc.py']
+else:
+    scripts = ['bin/twarc']
 
 if __name__ == "__main__":
-    if os.name == "nt":
-            setup(
-            name='twarc',
-            version=__version__,
-            url='http://github.com/docnow/twarc',
-            author='Ed Summers',
-            author_email='ehs@pobox.com',
-            py_modules=['twarc', ],
-            scripts=['twarc.py', 'utils/twarc-archive.py'],
-            description='command line utility to archive Twitter search results '
-                        'as line-oriented-json',
-            install_requires=dependencies,
-            setup_requires=['pytest-runner'],
-            tests_require=['pytest'],
-        )
-    else:
-        setup(
-            name='twarc',
-            version=__version__,
-            url='http://github.com/docnow/twarc',
-            author='Ed Summers',
-            author_email='ehs@pobox.com',
-            py_modules=['twarc', ],
-            scripts=['bin/twarc'],
-            description='command line utility to archive Twitter search results '
-                        'as line-oriented-json',
-            install_requires=dependencies,
-            setup_requires=['pytest-runner'],
-            tests_require=['pytest'],
-        )
+    setup(
+        name='twarc',
+        version=__version__,
+        url='http://github.com/docnow/twarc',
+        author='Ed Summers',
+        author_email='ehs@pobox.com',
+        py_modules=['twarc', ],
+        scripts=scripts,
+        description='Archive tweets from the command line',
+        install_requires=dependencies,
+        setup_requires=['pytest-runner'],
+        tests_require=['pytest'],
+    )
