bf5462ccd74b281e0e52ac07d56b3961c7d6ce9b
Ed Summers <ehs@pobox.com>
2017-03-10 17:05:47 -0500

    upped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os.path import join
 from setuptools import setup
 
 # Also in twarc.py
-__version__ = '1.0.7'
+__version__ = '1.0.8'
 
 if sys.version_info[0] < 3:
     dependencies = open(join('requirements', 'python2.txt')).read().split()
