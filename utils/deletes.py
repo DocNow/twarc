16c3a82fae5ff9047dba524d238fc41cfe8f94f4
Ed Summers <ehs@pobox.com>
2017-11-04 23:07:31 -0400

    folded deletes_api.py into deletes.py

--- utils/deletes.py
+++ utils/deletes.py
@@ -2,55 +2,68 @@
 
 """
 This program assumes that you are feeding it tweet JSON data for tweets
-that have been deleted. It will use the metadata and the live web to 
+that have been deleted. It will use the metadata and the API to
 analyze why each tweet appears to have been deleted.
+
+Note that lookups are based on user id, so may give different results than
+looking up a user by screen name.
 """
 
 import json
-import time
-import logging
 import fileinput
 import collections
-import urllib.request
+import requests
+import twarc
+import argparse
+import logging
 
-from urllib.error import HTTPError
-
-
-USER_OK                 = "USER_OK"
-USER_DELETED            = "USER_DELETED"
-USER_PROTECTED          = "USER_PROTECTED"
-USER_SUSPENDED          = "USER_SUSPENDED"
-TWEET_OK                = "TWEET_OK"
-TWEET_DELETED           = "TWEET_DELETED"
-RETWEET_DELETED         = "RETWEET_DELETED"
-ORIGINAL_TWEET_DELETED  = "ORIGINAL_TWEET_DELETED"
-ORIGINAL_USER_DELETED   = "ORIGINAL_USER_DELETED"
+USER_OK = "USER_OK"
+USER_DELETED = "USER_DELETED"
+USER_PROTECTED = "USER_PROTECTED"
+USER_SUSPENDED = "USER_SUSPENDED"
+TWEET_OK = "TWEET_OK"
+TWEET_DELETED = "TWEET_DELETED"
+RETWEET_DELETED = "RETWEET_DELETED"
+ORIGINAL_TWEET_DELETED = "ORIGINAL_TWEET_DELETED"
+ORIGINAL_USER_DELETED = "ORIGINAL_USER_DELETED"
 ORIGINAL_USER_PROTECTED = "ORIGINAL_USER_PROTECTED"
 ORIGINAL_USER_SUSPENDED = "ORIGINAL_USER_SUSPENDED"
 
+t = twarc.Twarc()
 
-def main():
+
+def main(files, enhance_tweet=False, print_results=True):
     counts = collections.Counter()
-    for line in fileinput.input():
+    for count, line in enumerate(fileinput.input(files=files)):
+        if count % 10000 == 0:
+            logging.info("processed {:,} tweets".format(count))
         tweet = json.loads(line)
         result = examine(tweet)
-        print(tweet_url(tweet), result)
+        if enhance_tweet:
+            tweet['delete_reason'] = result
+            print(json.dumps(tweet))
+        else:
+            print(tweet_url(tweet), result)
         counts[result] += 1
-    for result, count in counts.most_common():
-        print(result, count)
+    if print_results:
+        for result, count in counts.most_common():
+            print(result, count)
 
 
 def examine(tweet):
     user_status = get_user_status(tweet)
+    # Go with user status first (suspended, protected, deleted)
     if user_status != USER_OK:
         return user_status
     else:
         retweet = tweet.get('retweeted_status', None)
         tweet_status = get_tweet_status(tweet)
 
+        # If not a retweet and tweet deleted, then tweet deleted.
+
         if tweet_status == TWEET_OK:
             return TWEET_OK
-        elif retweet == None:
+        elif retweet is None:
             return tweet_status
         else:
             rt_status = examine(retweet)
@@ -66,79 +79,79 @@ def examine(tweet):
                 return RETWEET_DELETED
             else:
                 raise "Unexpected retweet status %s for %s" % (rt_status,
-                    tweet['id_str'])
+                                                               tweet['id_str'])
 
 
 users = {}
+
+
 def get_user_status(tweet):
-    screen_name = tweet['user']['screen_name']
-    if screen_name in users:
-        return users[screen_name]
+    user_id = tweet['user']['id_str']
+    if user_id in users:
+        return users[user_id]
 
-    url = "https://twitter.com/%s" % screen_name
+    url = "https://api.twitter.com/1.1/users/show.json"
+    params = {"user_id": user_id}
+
+    # USER_DELETED: 404 and {"errors": [{"code": 50, "message": "User not found."}]}
+    # USER_PROTECTED: 200 and user object with "protected": true
+    # USER_SUSPENDED: 403 and {"errors":[{"code":63,"message":"User has been suspended."}]}
+    result = USER_OK
     try:
-        resp = get(url)
-        html = resp.read().decode("utf8")
-
-        if resp.url == "https://twitter.com/account/suspended":
-            result = USER_SUSPENDED
-        elif "This account's Tweets are protected." in html:
+        resp = t.get(url, params=params, allow_404=True)
+        user = resp.json()
+        if user['protected']:
             result = USER_PROTECTED
-        elif resp.code == 200:
-            result = USER_OK
-    except HTTPError as error:
-        if error.code == 404:
+    except requests.exceptions.HTTPError as e:
+        try:
+            resp_json = e.response.json()
+        except json.decoder.JSONDecodeError:
+            raise e
+        if e.response.status_code == 404 and has_error_code(resp_json, 50):
             result = USER_DELETED
+        elif e.response.status_code == 403 and has_error_code(resp_json, 63):
+            result = USER_SUSPENDED
         else:
-            raise Exception("unexpected http status %s for %s" % (error.code,
-                url))
+            raise e
 
-    users[screen_name] = result
+    users[user_id] = result
     return result
 
 
 tweets = {}
+
+
 def get_tweet_status(tweet):
-    url = tweet_url(tweet)
-    if url in tweets:
-        return tweets[url]
+    id = tweet['id_str']
+    if id in tweets:
+        return tweets[id]
+    # USER_SUSPENDED: 403 and {"errors":[{"code":63,"message":"User has been suspended."}]}
+    # USER_PROTECTED: 403 and {"errors":[{"code":179,"message":"Sorry, you are not authorized to see this status."}]}
+    # TWEET_DELETED: 404 and {"errors":[{"code":144,"message":"No status found with that ID."}]}
+    # or {"errors":[{"code":34,"message":"Sorry, that page does not exist."}]}
 
+    url = "https://api.twitter.com/1.1/statuses/show.json"
+    params = {"id": id}
+
+    result = TWEET_OK
     try:
-        resp = get(url)
-        if "protected_redirect" in resp.url:
-            return USER_PROTECTED
-        elif resp.url == "https://twitter.com/account/suspended":
-            return USER_SUSPENDED
-        elif resp.code == 200:
-            result = TWEET_OK
-        else: 
-            raise Exception("unexpected http status %s for %s" % (error.code,
-                url))
-    except HTTPError as error:
-        if error.code == 404:
-            return TWEET_DELETED
+        t.get(url, params=params, allow_404=True)
+    except requests.exceptions.HTTPError as e:
+        try:
+            resp_json = e.response.json()
+        except json.decoder.JSONDecodeError:
+            raise e
+        if e.response.status_code == 404 and has_error_code(resp_json, (34, 144)):
+            result = TWEET_DELETED
+        elif e.response.status_code == 403 and has_error_code(resp_json, 63):
+            result = USER_SUSPENDED
+        elif e.response.status_code == 403 and has_error_code(resp_json, 179):
+            result = USER_PROTECTED
         else:
-            raise Exception("unexpected http status %s for %s" % (error.code,
-                url))
-   
-    tweets[url] = result
+            raise e
+
+    tweets[id] = result
     return result
-
-
-UA = "twarc-deletes (+https://github.com/DocNow/twarc/blob/master/utils/deletes.py)"
-
-def get(url):
-    time.sleep(.5)
-    req = urllib.request.Request(url, headers={'User-Agent': UA})
-    try:
-        return urllib.request.urlopen(req)
-    except HTTPError as error:
-        if error.code == 503:
-            logging.warn("got 503 on %s", url)
-            time.sleep(10)
-            return get(url)
-        else:
-            raise(error)
 
 
 def tweet_url(tweet):
@@ -146,5 +159,22 @@ def tweet_url(tweet):
         tweet['user']['screen_name'], tweet['id_str'])
 
 
+def has_error_code(resp, code):
+    if isinstance(code, int):
+        code = (code, )
+    for error in resp['errors']:
+        if error['code'] in code:
+            return True
+    return False
+
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--enhance', action='store_true',
+                        help='Enhance tweet with delete_reason and output enhanced tweet.')
+    parser.add_argument('--skip-results', action='store_true', help='Skip outputting delete reason summary')
+    parser.add_argument('files', metavar='FILE', nargs='*', help='files to read, if empty, stdin is used')
+    args = parser.parse_args()
+
+    main(args.files if len(args.files) > 0 else ('-',), enhance_tweet=args.enhance,
+         print_results=not args.skip_results and not args.enhance)
