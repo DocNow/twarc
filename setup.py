990a20b9dbf3a47335c80297015366eab303a4e6
Ed Summers <ehs@pobox.com>
2020-07-06 16:00:15 -0400

    allow access key/secret to be entered manually during configure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import join
 from setuptools import setup
 
 # Also in twarc/__init__.py
-__version__ = '1.10.0'
+__version__ = '1.10.1'
 
 with open("README.md") as f:
     long_description = f.read()
--- twarc/client.py
+++ twarc/client.py
@@ -1057,42 +1057,58 @@ class Twarc(object):
             self.consumer_key = get_input('consumer key: ')
             self.consumer_secret = get_input('consumer secret: ')
 
-        request_token_url = 'https://api.twitter.com/oauth/request_token'
-        oauth = OAuth1(self.consumer_key, client_secret=self.consumer_secret)
-        r = requests.post(url=request_token_url, auth=oauth)
+        answered = False
+        while not answered:
+            print("\nHow would you like twarc to obtain your user keys?\n\n1) generate access keys by visiting Twitter\n2) manually enter your access token and secret\n")
+            answer = get_input('Please enter your choice [1/2] ')
+            if answer == "1":
+                answered = True
+                generate = True
+            elif answer == "2":
+                answered = True
+                generate = False
 
-        credentials = parse_qs(r.text)
-        if not credentials:
-            print("\nError: invalid credentials.")
-            print("Please check that you are copying and pasting correctly and try again.\n")
-            return
+        if generate:
+            request_token_url = 'https://api.twitter.com/oauth/request_token'
+            oauth = OAuth1(self.consumer_key, client_secret=self.consumer_secret)
+            r = requests.post(url=request_token_url, auth=oauth)
 
-        resource_owner_key = credentials.get('oauth_token')[0]
-        resource_owner_secret = credentials.get('oauth_token_secret')[0]
+            credentials = parse_qs(r.text)
+            if not credentials:
+                print("\nError: invalid credentials.")
+                print("Please check that you are copying and pasting correctly and try again.\n")
+                return
 
-        base_authorization_url = 'https://api.twitter.com/oauth/authorize'
-        authorize_url = base_authorization_url + '?oauth_token=' + resource_owner_key
-        print('\nPlease log into Twitter and visit this URL in your browser:\n%s' % authorize_url)
-        verifier = get_input('\nAfter you have authorized the application please enter the displayed PIN: ')
+            resource_owner_key = credentials.get('oauth_token')[0]
+            resource_owner_secret = credentials.get('oauth_token_secret')[0]
 
-        access_token_url = 'https://api.twitter.com/oauth/access_token'
-        oauth = OAuth1(self.consumer_key,
-                       client_secret=self.consumer_secret,
-                       resource_owner_key=resource_owner_key,
-                       resource_owner_secret=resource_owner_secret,
-                       verifier=verifier)
-        r = requests.post(url=access_token_url, auth=oauth)
-        credentials = parse_qs(r.text)
+            base_authorization_url = 'https://api.twitter.com/oauth/authorize'
+            authorize_url = base_authorization_url + '?oauth_token=' + resource_owner_key
+            print('\nPlease log into Twitter and visit this URL in your browser:\n%s' % authorize_url)
+            verifier = get_input('\nAfter you have authorized the application please enter the displayed PIN: ')
 
-        if not credentials:
-            print('\nError: invalid PIN')
-            print('Please check that you entered the PIN correctly and try again.\n')
-            return
+            access_token_url = 'https://api.twitter.com/oauth/access_token'
+            oauth = OAuth1(self.consumer_key,
+                           client_secret=self.consumer_secret,
+                           resource_owner_key=resource_owner_key,
+                           resource_owner_secret=resource_owner_secret,
+                           verifier=verifier)
+            r = requests.post(url=access_token_url, auth=oauth)
+            credentials = parse_qs(r.text)
 
-        self.access_token = resource_owner_key = credentials.get('oauth_token')[0]
-        self.access_token_secret = credentials.get('oauth_token_secret')[0]
+            if not credentials:
+                print('\nError: invalid PIN')
+                print('Please check that you entered the PIN correctly and try again.\n')
+                return
 
-        screen_name = credentials.get('screen_name')[0]
+            self.access_token = resource_owner_key = credentials.get('oauth_token')[0]
+            self.access_token_secret = credentials.get('oauth_token_secret')[0]
+
+            screen_name = credentials.get('screen_name')[0]
+        else:
+            self.access_token = get_input("Enter your Access Token: ")
+            self.access_token_secret = get_input("Enter your Access Token Secret: ")
+            screen_name = "default"
 
         config = self.save_config(screen_name)
         print('\nThe credentials for %s have been saved to your configuration file at %s' % (screen_name, self.config))
