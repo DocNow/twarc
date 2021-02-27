708c5510c55d7d46146ebceed7121270556f6240
Ed Summers <ehs@pobox.com>
2021-02-27 08:21:15 -0500

    foaf.py fixes
    The code to actually walk the network was commented out for some reason,
    perhaps for debugging at some point. That has been fixed, and the
    internal documentation has been updated to better reflect what is going
    on.
    
    refs #392

--- utils/foaf.py
+++ utils/foaf.py
@@ -2,15 +2,12 @@
 
 """
 This is a utility for getting the friend-of-a-friend network for a 
-given twitter user. The network is expressed as tuples of user identifiers for
-the user and their friend (who they follow).
+given twitter user. It writes a sqlite database as it collects the data
+{user-id}.sqlite and once complete it exports that data to two csv files:
 
-User identifiers are used rather than the handles or screen_name, since the 
-handles can change, and Twitter's API allows you to get friends as ids much 
-faster.
+* {user-id}.csv - the user id links
+* {user-id}-users.csv - metadata about the users keyed off their user id
 
-You can of course turn the IDs back into usernames later if you want using
-twarc.
 """
 
 import re
@@ -28,45 +25,6 @@ logging.basicConfig(
     filename="foaf.log",
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(message)s"
-)
-
-parser = argparse.ArgumentParser("tweet.py")
-parser.add_argument("user", action="store", help="user_id")
-parser.add_argument("--level", type=int, action="store", default=2, 
-        help="how far out into the social graph to follow")
-
-args = parser.parse_args()
-
-# create twarc instance for querying Twitter
-t = twarc.Twarc()
-
-# setup sqlite db for storing information as it is collected
-db = sqlite3.connect('foaf.sqlite3')
-
-db.execute(
-    '''
-    CREATE TABLE IF NOT EXISTS friends (
-      user_id INT,
-      friend_id INT,
-      PRIMARY KEY (user_id, friend_id)
-    )
-    '''
-)
-
-db.execute(
-    '''
-    CREATE TABLE IF NOT EXISTS users (
-      user_id INT,
-      screen_name TEXT,
-      name TEXT,
-      description TEXT,
-      location TEXT,
-      created TEXT,
-      statuses INT,
-      verified TEXT,
-      PRIMARY KEY (user_id)
-    )
-    '''
 )
 
 
@@ -164,33 +122,65 @@ def add_user(u):
     )
     db.commit()
 
+# get command line arguments
+parser = argparse.ArgumentParser("tweet.py")
+parser.add_argument("user", action="store", help="user_id")
+parser.add_argument("--level", type=int, action="store", default=2, 
+        help="how far out into the social graph to follow")
+args = parser.parse_args()
 
-# lookup friendship data
+# create twarc instance for querying Twitter
+t = twarc.Twarc()
+
+# get the seed user_id, potentially from their screen name
 if re.match("^\d+$", args.user):
     seed_user_id = args.user
 else:
     seed_user_id = next(t.user_lookup([args.user]))['id_str']
 
-"""
+# setup sqlite db for storing information as it is collected
+db = sqlite3.connect(f'{seed_user_id}.sqlite3')
+db.execute(
+    '''
+    CREATE TABLE IF NOT EXISTS friends (
+      user_id INT,
+      friend_id INT,
+      PRIMARY KEY (user_id, friend_id)
+    )
+    '''
+)
+db.execute(
+    '''
+    CREATE TABLE IF NOT EXISTS users (
+      user_id INT,
+      screen_name TEXT,
+      name TEXT,
+      description TEXT,
+      location TEXT,
+      created TEXT,
+      statuses INT,
+      verified TEXT,
+      PRIMARY KEY (user_id)
+    )
+    '''
+)
+
+# lookup friendship data
 for friendship in friendships(seed_user_id, args.level):
     print("%s,%s" % friendship)
-"""
 
 # lookup user metadata
 for user in t.user_lookup(user_ids()):
     add_user(user)
 
 # write out friendships
-
 with open('{}.csv'.format(seed_user_id), 'w') as fh:
     w = csv.writer(fh)
     w.writerow(['user_id', 'friend_user_id'])
     for row in db.execute('SELECT * FROM friends'):
         w.writerow(row)
 
-
 # write out user data as csv
-
 with open('{}-users.csv'.format(seed_user_id), 'w') as fh:
     w = csv.writer(fh)
     w.writerow([
