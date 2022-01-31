392c1724784a21a18afa1b50dfc7e70615db3e86
Ed Summers <ehs@pobox.com>
2022-01-31 09:44:41 -0500

    new version

--- twarc/version.py
+++ twarc/version.py
@@ -1,5 +1,5 @@
 import platform
 
-version = "2.9.1"
+version = "2.9.2"
 
 user_agent = f"twarc/{version} ({platform.system()} {platform.machine()}) {platform.python_implementation()}/{platform.python_version()}"
