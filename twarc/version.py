9092069ffcde289570abf9bde5d37037884f33ea
Sam Hames <sam.hames@qut.edu.au>
2022-04-21 15:01:32 +1000

    Bump version to release bugfixes

--- twarc/version.py
+++ twarc/version.py
@@ -1,5 +1,5 @@
 import platform
 
-version = "2.10.2"
+version = "2.10.3"
 
 user_agent = f"twarc/{version} ({platform.system()} {platform.machine()}) {platform.python_implementation()}/{platform.python_version()}"
