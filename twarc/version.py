2039d3b983242571def5c69a2e2bf714f9fba808
Sam Hames <s.hames@uq.edu.au>
2022-09-30 14:36:14 +1000

    Add additional fields to collect tweet edit history information
    - Also bump version for release

--- twarc/version.py
+++ twarc/version.py
@@ -1,5 +1,5 @@
 import platform
 
-version = "2.11.3"
+version = "2.12.0"
 
 user_agent = f"twarc/{version} ({platform.system()} {platform.machine()}) {platform.python_implementation()}/{platform.python_version()}"
