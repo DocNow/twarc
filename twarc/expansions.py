a8007b2a151880e367960dcf0c4953810918eca6
Igor Brigadir <igor.brigadir@gmail.com>
2022-12-20 22:09:57 +0000

    add missing expansion for edited tweets

--- twarc/expansions.py
+++ twarc/expansions.py
@@ -18,6 +18,7 @@ EXPANSIONS = [
     "in_reply_to_user_id",
     "referenced_tweets.id",
     "referenced_tweets.id.author_id",
+    "edit_history_tweet_ids"
     "entities.mentions.username",
     "attachments.poll_ids",
     "attachments.media_keys",
